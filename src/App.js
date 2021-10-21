92c364eee7b380c21d3cde4daee1fdc542e4c818
Eric R <eric92rodriguez@gmail.com>
2021-10-21 20:17:54 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
+import Footer from './Footer';
+import Home from './Home';
+import Menu from './Menu';
+import Registro from './Registro';
+import Loguin from './Loguin';
+import Detalle from './Detalle';
+import NotFoundPage from './NotFoundPage';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Menu/>
+      <Switch>
+        <Router path="/registro" exact>
+          <Registro />
+        </Router>
+        <Router path="/loguin" exact>
+          <Loguin />
+        </Router>
+        <Router path="/detalle" exact>
+          <Detalle />
+        </Router>
+        <Route path="/" exact>
+          <Home/>
+        </Route>
+        <Redirect from="/home" to="/" exact />
+        <Route path="*">
+          <NotFoundPage/>
+        </Route>
+      </Switch>
+      <Footer/>
+    </Router>
   );
 }
 
