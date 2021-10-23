4aefcf8e073c2e266fead2ccb9814556890deddb
Eric R <eric92rodriguez@gmail.com>
2021-10-23 01:15:49 -0300

    Create Buy.js

---
+++ src/Buy.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function Buy() {
+  return (
+    <div class="color">
+    <div class="row">
+        <div class="col-12 col-md p-5">
+        <main role="main" class="inner cover">
+        <h1 class="cover-heading">Gracias por su compra!</h1>
+        <p class="lead">
+            <a href="/" class="btn btn-lg btn-secondary col-md-5 mx-auto m-3 d-flex">Seguir comprando</a>
+        </p>
+        </main>
+        </div>
+    </div>
+    </div>
+  );
+}
+
+export default Buy;
